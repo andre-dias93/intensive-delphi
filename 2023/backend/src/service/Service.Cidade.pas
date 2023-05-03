0378b3fbcc4f192eebd4acf52e46d57d66f36890
andre.dias <anndrelluizdiias@gmail.com>
2023-05-02 23:45:58 -0300

    Intensive Delphi 2023

---
+++ 2023/backend/src/service/Service.Cidade.pas
@@ -0,0 +1,25 @@
+unit Service.Cidade;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Service.Base.Simples, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.Phys.FB, FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, frxClass, frxDBSet, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client, frxExportBaseDialog, frxExportPDF;
+
+type
+  TServiceCidade = class(TServiceBaseSimples)
+    qryReportID: TIntegerField;
+    qryReportNAME: TWideStringField;
+    qryReportCOUNTRY: TWideStringField;
+    qryReportADDRESS: TWideStringField;
+  end;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ 2023/frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+      <div style="padding: 10px">
+          <mat-card style="box-shadow: 0 0.5rem 1rem rgb(0 0 0 / 13%)">
+              <h2>Intensive Delphi 2023!</h2>
+              <span>Relatórios via APIs com <strong>Horse</strong> e <strong>Angular</strong>!</span>
+          </mat-card>
+      </div>`,
+})
+export class HomeComponent {
+}
